85252630c69402c41e095a6872c24ecdf0baffb1
Henri <henri.tuan77@gmail.com>
2025-02-09 12:14:23 +0100

    feat: client mock repository

---
+++ src/modules/client/infrastructure/persistence/client.mock-repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+import { Client } from '../../domain/client.entity';
+import { IClientRepository } from '../../domain/ports/repository.interface';
+
+@Injectable()
+export class ClientMockRepository implements IClientRepository {
+  findById(id: number) {
+    if (id > 26) {
+      return Promise.reject(new Error(`Client not found with id ${id}`));
+    }
+
+    const mockClient = new Client({
+      id,
+      firstName: 'John',
+      lastName: 'Doe',
+    });
+
+    console.log('Returning mock Client');
+    return Promise.resolve(mockClient);
+  }
+}
